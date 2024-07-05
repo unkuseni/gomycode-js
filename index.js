bdab79257ec552d86487d1949d6bf698657b5015
unkuseni <amudalatadeoye@gmail.com>
2024-07-05 16:32:25 +0100

    edabit done

---
+++ index.js
@@ -0,0 +1,91 @@
+function hello() {
+	return "Hello edabit.com";
+}
+
+function circuitPower(x, y) {
+	return x * y;
+}
+
+function add(a, b) {
+	return a + b;
+}
+
+function cubes(x) {
+	return x * x * x;
+}
+
+function increment(x) {
+	return x + 1;
+}
+
+function maxEdge(x, y) {
+	return x + y - 1;
+}
+
+function toSeconds(x) {
+	return x * 60;
+}
+
+function triArea(a, b) {
+	return (a * b) / 2;
+}
+
+function ballPoints(x, y) {
+	return x * 2 + y * 3;
+}
+
+function addSomething(x) {
+	return `something ${x}`;
+}
+
+function modulo(x, y) {
+	return x % y;
+}
+
+function ageToDays(x) {
+	return x * 365;
+}
+
+function rectPerimeter(x, y) {
+	return 2 * (x + y);
+}
+
+function polygonAngles(x) {
+	return (x - 2) * 180;
+}
+
+function animalLegs(x, y, z) {
+	return x * 2 + y * 4 + z * 4;
+}
+
+function returnFirst(x) {
+	return x[0];
+}
+
+function addEdabit(x) {
+  return x + "Edabit";
+}
+
+function lessThan100(a, b) {
+  return a + b < 100;
+}
+
+function squared(x) {
+  return x * x;
+}
+
+function and(x, y) {
+  return x && y;
+}
+
+function isSameNum(x, y) {
+  return x === y;
+}
+
+function hoursToSeconds(x) {
+  return x * 60 * 60;
+}
+
+function swap(x, y) {
+  return [y, x];
+}
